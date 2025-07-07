512eb27e19a14edcd87cf6cd3db8a886702963b3
sunnyday0206 <sunme8412@gmail.com>
2025-07-07 19:11:43 +0900

    chore(#1): Initial Setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('MistFarm')
+    .setDescription('지유인가요, 지호인가요?')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('Your API Tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
