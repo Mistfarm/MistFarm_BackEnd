cfd26cb24e177313b48f446131c509d583d84e17
ajswldp <kkhhjj1209@gmail.com>
2025-08-05 19:36:14 +0900

    fix(#2): update auth API documentation

--- src/auth/dto/auth.password.update.dto.ts
+++ src/auth/dto/auth.password.update.dto.ts
@@ -5,6 +5,7 @@ export class AuthPasswordUpdateDto {
   @IsString()
   @ApiProperty({ example: 'test123', description: '현제 비밀번호' })
   oldPassword: string;
+
   @IsString()
   @ApiProperty({ example: 'test1234', description: '새로운 비밀번호' })
   newPassword: string;
