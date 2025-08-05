16219c6815cfc4a117b4526a67665c5716ba346f
ajswldp <kkhhjj1209@gmail.com>
2025-08-05 19:16:59 +0900

    feat(#2): add auth API documentation

--- src/auth/dto/auth.name.dto.ts
+++ src/auth/dto/auth.name.dto.ts
@@ -1,7 +1,12 @@
 import { IsString, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthNameDto {
   @IsString()
+  @ApiProperty({
+    example: '김먼지',
+    description: '공백, 특수문자, 숫자 없이 2자 이상 25자 이하의 이름',
+  })
   @Matches(/^[가-힣a-zA-Z]{2,25}$/, {
     message:
       '이름은 공백, 특수문자, 숫자 없이 2자 이상 25자 이하로 입력해주세요.',
