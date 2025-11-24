0810aa0dcb609439fa11f13ceeac502d3b39e9b6
sunnyday0206 <sunme8412@gmail.com>
2025-11-24 22:01:53 +0900

    feat(#21): PLANT_TYPE 종류 변경

--- src/domain/zone/plant.types.ts
+++ src/domain/zone/plant.types.ts
@@ -1,2 +1,2 @@
-export const PLANT_TYPES = ['gosary', 'lettuce', 'none'] as const;
+export const PLANT_TYPES = ['gosary', 'hub', 'none'] as const;
 export type PlantType = (typeof PLANT_TYPES)[number];
